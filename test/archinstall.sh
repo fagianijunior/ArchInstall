66f0dc416b3864fec16e0d072ac6732fc08b2b89
Carlos Fagiani Junior <fagianijunior@gmail.com>
2013-01-08 21:16:59 +0000

    Limpesa

--- test/archinstall.sh
+++ test/archinstall.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
-########################################################################
-# This program is free software: you can redistribute it and/or modify #
-# it under the terms of the GNU General Public License as published by #
-# the Free Software Foundation, either version 3 of the License, or    #
-# (at your option) any later version.                                  #
-#                                                                      #
-# This program is distributed in the hope that it will be useful,      #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of       #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        #
-# GNU General Public License for more details.                         #
-#                                                                      #
-# You should have received a copy of the GNU General Public License    #
-# along with this program.  If not, see <http://www.gnu.org/licenses/> #
-#                                                                      #
+
 # Author: Carlos Fagiani Junior                                        #
 # E-mail: fagianijunior@gmail.com                                      #
-# Versão: 1.2                                                          #
-#                                                                      #
+# Versão: 0.2                                                          #
+
 # Este Script segue o guia de instalação Arch:                         #
 # https://wiki.archlinux.org/index.php/Installation_Guide              #
-#                                                                      #
 ########################################################################
 
 # 1- Rode o CD/PenDrive de instalação do Arch Linux
@@ -70,13 +56,11 @@ function espera() {
 	unset a;
 }
 
-# Configura o teclado
 loadkeys $layout_teclado;
 espera "Teclado Configurado.";
 
 # Verifica se as informações estão corretas #
 
-#Verifica se o usuário realmente alterou os dados
 if [ "$alterei_os_dados_acima" == "nao" ]; then
    echo "Antes de rodar o script edite os dados internos. Operação cancelada.";
    exit;
@@ -97,7 +81,6 @@ if [ ! -e "$boot_hd" ] || [ ! -e "$swap_hd" ] || [ ! -e "$root_hd" ] || [ ! -e "
    fi
 fi
 
-# Formata as partições root, boot e SWAP
 mkfs -t ext2 $boot_hd;
 espera "$boot_hd formatado.";
 mkswap $swap_hd;
@@ -110,7 +93,6 @@ if [ "$formatar_home_hd" == "sim" ]; then
    espera "$home_hd formatado.";
 fi
 
-# Monta as partições e a SWAP
 swapon $swap_hd;
 espera "SWAP ligada.";
 mount $root_hd /mnt;
@@ -122,52 +104,42 @@ espera "$boot_hd montado em /mnt/boot";
 mount $home_hd /mnt/home;
 espera "$home_hd montado em /mnt/home";
 
-# Caso for utilizar o wifi
 if [ "$usar_wifi" == "sim" ]; then
 	wifi-menu;
 	espera "Wifi conectado.";
 fi
 
-# Instala a base do sistema
 pacstrap /mnt base base-devel wpa_supplicant dialog;
 espera "base e base-devel instalados.";
 
-# Instala o bootloader
 if [ "$boot_loader" == "grub" ]; then
 	pacstrap /mnt grub-bios;
-elif [ "$boot_loader" == "syslinux" ]; then 
+elif [ "$boot_loader" == "syslinux" ]; then
 	pacstrap /mnt syslinux;
 fi
 espera "$boot_loader bootloader instalado.";
 
-# Cria o /etc/fstab
 genfstab -p /mnt >> /mnt/etc/fstab;
 cat /mnt/etc/fstab;
 espera "fstab gerado.";
 
-# Hostname
 echo $hostname > /mnt/etc/hostname;
 espera "Adicionou $hostname em /etc/hostname";
 
-# Localtime
 arch-chroot /mnt /bin/bash -c "ln -s /usr/share/zoneinfo/$localtime /etc/localtime;";
 espera "Configurou localtime.";
 
-# Tradução
 echo "LANG="$linguagem > /mnt/etc/locale.conf;
 espera "Criou o arquivo locale.gen.";
 
-# Configura texto no console
 echo "KEYMAP="$layout_teclado > /mnt/etc/vconsole.conf;
 echo "FONT="$font >> /mnt/etc/vconsole.conf;
 echo "FONT_MAP="$font_map >> /mnt/etc/vconsole.conf;
 espera "Configurou o vconsole.conf.";
 
-# Configura o locale.gen
 cp /mnt/etc/locale.gen /mnt/tmp/locale.gen;
 sed "s/#"$linguagem"/"$linguagem"/g" /mnt/tmp/locale.gen > /mnt/etc/locale.gen;
 
-# Entra no sistema instalado
 arch-chroot /mnt /bin/bash -c "locale-gen";
 arch-chroot /mnt /bin/bash -c "mkinitcpio -p linux";
 espera "gerou o locale. Criou a RAM disk.";
@@ -183,20 +155,20 @@ elif [ "$boot_loader" == "syslinux" ]; then
 fi
 espera "Configurou o $boot_loader";
 
-#Experimental
 if [ "$pos_instalacao" == "sim" ]; then
    pacstrap /mnt wget;
    arch-chroot /mnt /bin/bash -c "wget https://github.com/fagianijunior/ArchInstall/blob/master/src/pos_archinstall.sh -O /root/pos_archinstall.sh";
    espera "Quando reiniciar o sistema execute o script pos_archinstall.sh que foi gerado na pasta /root";
 fi
 
-#Cria senha do ROOT
-arch-chroot /mnt /bin/bash -c "passwd";
+arch-chroot /mnt /bin/bash -c "passwd << EOF
+$root_senha
+$root_senha
+EOF";
 espera "Setou a senha do ROOT.";
 
-# Desmonta as partições
 umount /mnt/{boot,home,};
 espera "Desmontou as partições.";
 
-echo "Seu novo Arch Linux está instalado e com as configurações básicas.";
+espera "Seu novo Arch Linux está instalado e com as configurações básicas.";
 exit;
